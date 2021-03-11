dc7f6de88ae04c4242be8e50958f1f314098d4c7
DrMint <thom.barillot@gmail.com>
2021-03-11 18:43:47 +0100

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,8 @@
-export function homeURL() {return        "https://okuma.r-entries.com"             }
-export function readerURL() {return      "https://okuma.r-entries.com/read.html"   }
-export function booksURL() {return       "https://okuma.r-entries.com/books/"        }
-export function websiteName() {return    "OKUMA"                                   }
+/*  PLEASE CONFIGURE THOSE VALUES OR OKUMA WON'T WORK.
+    booksURL can be on a different domain/subdomain if you prefer.
+    The books are stored in a library. The library name can be whatever you want (here it's books)
+*/
+export function homeURL() {return        "https://domain.com/"}
+export function readerURL() {return      "https://domain.com/read.html"}
+export function booksURL() {return       "https://domain.com/books/"}
+export function websiteName() {return    "WEBSITE TITLE"}
