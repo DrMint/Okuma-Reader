654802823a6199b228e01ff76edf5a03e7bbb692
DrMint <thom.barillot@gmail.com>
2021-10-23 19:05:31 +0200

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -2,8 +2,8 @@
     booksURL can be on a different domain/subdomain if you prefer.
     The books are stored in a library. The library name can be whatever you want (here it's books)
 */
-export function homeURL() {return           "https://domain.com/"}
-export function readerURL() {return         "https://domain.com/read.html"}
-export function booksURL() {return          "https://domain.com/books/"}
-export function websiteName() {return       "WEBSITE TITLE"}
+export function homeURL() {return           "https://drmint.github.io/Okuma-Reader/"}
+export function readerURL() {return         "https://drmint.github.io/Okuma-Reader/read.html"}
+export function booksURL() {return          "https://drmint.github.io/Okuma-Reader/books/"}
+export function websiteName() {return       "Okuma Example"}
 export function defaultLanguage() {return   "en"}
